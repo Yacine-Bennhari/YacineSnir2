ffec30346e5ce0538f24d8c59ae0ed2063ddfa13
YacineSnir2 <112879006+YacineSnir2@users.noreply.github.com>
2022-09-15 10:31:29 +0200

    Add files via upload

---
+++ Tp3.py
@@ -0,0 +1,13 @@
+# x = int(input("Tapez la valeur de x "))
+# print("La table de multiplication de : ", x," est :")
+# for i in range(1,11):
+#     print(i , " x ", x, " = ",i*x)
+
+x = int(input("Tapez la valeur de x "))
+i = 0
+print("La table de multiplication de : ", x," est :")
+while i<=10:
+    print(i , " x ", x, " = ",i*x)
+    i += 1
+    
+    
---
+++ Tp3Exo2.py
@@ -0,0 +1,7 @@
+n=int(input('donner n'))
+for x in range(0,n+1):
+	print (x*"*")
+
+n=int(input('donner n'))
+for x in range(n,0,-1):
+	print (x*"*")
